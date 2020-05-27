0942c16c78b289e8550e911e88473c9dc64de698
Clark Evans <cdevans@gmail.com>
2020-05-27 19:50:12 -0400

    Update aliases

--- aliases.zsh
+++ aliases.zsh
@@ -7,15 +7,27 @@ alias reloadshell="source $HOME/.zshrc"
 # Directories
 alias dotfiles="cd $DOTFILES"
 alias library="cd $HOME/Library"
-alias projects="cd $HOME/Projects"
+alias dl="cd $HOME/Downloads"
+alias dt="cd $HOME/Desktop"
+alias proj="cd $HOME/Projects"
 
-# JS
-alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
-alias watch="npm run watch"
+# Flush Directory Service cache
+alias flush="dscacheutil -flushcache && killall -HUP mDNSResponder"
+
+# Show/hide hidden files in Finder
+alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
+
+# Print each PATH entry on a separate line
+alias path='echo -e ${PATH//:/\\n}'
 
 # Git
 alias amend="git commit --amend --no-edit"
 alias unstage="git restore --staged ."
+
+# JS
+alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
+alias watch="npm run watch"
 
 # RSPNDR
 alias rspndr-uuid="uuidgen | tr \"[:upper:]\" \"[:lower:]\" | pbcopy"
