e7ff4ba6ff8e8bbc1cbdaa6e0da88cf5dd2e087d
Clark Evans <cdevans@gmail.com>
2020-05-27 20:00:00 -0400

    Update functions

--- functions.zsh
+++ functions.zsh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-function gi() { curl curl -L -s https://www.gitignore.io/api/$@ ;}
+function gi() {
+    curl curl -L -s https://www.gitignore.io/api/$@ ;
+}
 
+function mkd() {
+    mkdir -p "$@" && cd "$_";
+}
