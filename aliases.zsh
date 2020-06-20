8c2b5cb1459a08113f95a79dcadc8290dbb1b540
Clark Evans <cdevans@gmail.com>
2020-06-19 21:57:56 -0400

    Update shebang

--- aliases.zsh
+++ aliases.zsh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 # Shortcuts
 alias myip="curl ifconfig.io"
--- functions.zsh
+++ functions.zsh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 function gi() {
     curl curl -L -s https://www.gitignore.io/api/$@ ;
